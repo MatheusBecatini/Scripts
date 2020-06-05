646698e013bf16171285bee7b76f6f63f6751cb2
Matheus Becatini <60360393+matheus-becatini@users.noreply.github.com>
2020-06-05 12:19:27 -0300

    React Simple Page - Create

---
+++ React-Simple-Page/src/components/Header/index.js
@@ -0,0 +1,3 @@
+import Header from './view/header'
+
+export default Header
---
+++ React-Simple-Page/src/components/Header/view/header.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+import '../style.css'
+
+const Header = () => {
+    return <header id="main-header">React Simple Page</header>
+}
+
+export default Header;
