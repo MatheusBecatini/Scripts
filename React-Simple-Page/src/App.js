1e4993080d4df6509c7023c68b598f79acd70cb9
Matheus Becatini <60360393+matheus-becatini@users.noreply.github.com>
2020-06-09 20:23:21 -0300

    React Simple Page request API

--- React-Simple-Page/src/App.js
+++ React-Simple-Page/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import "./style.css"
 import Header from './components/Header'
+import Main from './components/Main'
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Main></Main>
     </div>
   );
 }
